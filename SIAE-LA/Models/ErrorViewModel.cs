089f6e50eb5cd7b215efbc15779783ddc51b2f08
phantomjoker <phantomjoker@MIKAZUKIKOTETSU>
2025-08-25 03:03:30 -0600

    Add project files.

---
+++ SIAE-LA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIAE_LA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
